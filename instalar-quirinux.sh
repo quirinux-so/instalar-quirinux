9fc441d04572dff676727f434c2204a75b0dbd76
Quirinux GNU/Linux <49002514+quirinux-so@users.noreply.github.com>
2021-09-04 13:42:46 +0200

    Update instalar-quirinux.sh

--- instalar-quirinux.sh
+++ instalar-quirinux.sh
@@ -1349,17 +1349,6 @@ sudo apt-get autoremove --purge -y
 
 function _baseBusterPro() {
 
-
-FILE="/opt/requisitos/ok-bullseye"
-
-if [ ! -e ${FILE} ]; then
-
-mypaintq qstopmotion -y
-
-fi
-
-else
-
 # INSTALAR PAQUETES ESPECIALIZADOS DESDE BUSTER (KRITA, OBS, SYNFIG, XSANE, ETC)
 clear
 for paquetes_estandar in manuskript sweethome3d kdenlive guvcview xsane digikam k3d gnome-color-manager aegisub dispcalgui birdfont skanlite pencil2d devede vokoscreen-ng soundconverter hugin calf-plugins invada-studio-plugins-ladspa vlc-plugin-fluidsynth fluidsynth synfig synfigstudio synfig-examples pikopixel.app entangle darktable rawtherapee krita krita-data krita-gmic krita-l10n dvd-styler obs-studio obs-plugins gir1.2-entangle-0.1; do sudo apt-get install -y $paquetes_estandar; done
